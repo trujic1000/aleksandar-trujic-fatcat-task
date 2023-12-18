f8206ae8c3cf54f50f4bf7f49881c26bf64e099d
Aleksandar Trujic <atrujic@protonmail.com>
2023-12-18 12:38:35 +0100

    fix linter errors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import { type Config } from 'tailwindcss';
+
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
@@ -43,4 +44,4 @@ export default {
         },
     },
     plugins: [],
-};
+} satisfies Config;
